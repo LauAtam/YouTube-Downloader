6b4fec9d6f9620b67f633b4378fa2a29ac7a05a9
LauAtam <99442669+LauAtam@users.noreply.github.com>
2023-02-19 20:40:23 -0300

    path add

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pytube import YouTube
 
-def download(link):
+def download(link, path):
     ytObject = YouTube(link)
     ytObject = ytObject.streams.get_highest_resolution()
     try:
-        ytObject.download()
+        ytObject.download(output_path= path)
     except:
         print("Ha ocurrido un error")
     print("La descarga se hizo de forma exitosa")
